a56949370a181a20762064d10a17ee54d97cf279
Jaeeun98 <wodms4525@naver.com>
2021-06-04 18:04:44 +0900

    Title : Change
    position move → translate로 변경(렌더링에 맞춰서 layout 변경이 일어나지 않게 하려고)

--- coordinates/main.js
+++ coordinates/main.js
@@ -2,24 +2,26 @@ const target = document.querySelector('.target');
 const text = document.querySelector('.text');
 const xLine = document.querySelector('.xLine');
 const yLine = document.querySelector('.yLine');
-
-
-
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
 
 document.addEventListener('mousemove', (e) => {
   const x = e.clientX;
   const y = e.clientY;
 
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  xLine.style.top = `${y}px`;
-  yLine.style.left = `${x}px`;
+  target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+
+  xLine.style.transform = `translateY(${y}px)`;
+  yLine.style.transform = `translateX(${x}px)`;
   
   text.innerText = `${x}px, ${y}px`;
-  text.style.left = `${x}px`;
-  text.style.top = `${y}px`;
-
- 
-
-      
+  text.style.transform = `translate(${x - 40}px, ${y + 20}px)`;
+   
  })
+
+
+ /*
+ 
+ why position이 반드시 있어야 하는지
+ */

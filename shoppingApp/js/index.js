929ce851eba39feb17db3cd2a4079e19868f0d04
Jaeeun98 <wodms4525@naver.com>
2021-06-07 16:40:08 +0900

    Title : Add
    js, img click시 노드 삭제 add

--- shoppingApp/js/index.js
+++ shoppingApp/js/index.js
@@ -5,17 +5,24 @@ const section = document.getElementsByTagName('section')[0];
 
 form.addEventListener('submit', (e) => {
     const div = document.createElement('div');
-    const del = document.createElement('button');
+    const span = document.createElement('span');
+    const del = document.createElement('img');
 
-    div.innerText = input.value;
-    
+    span.innerText = input.value;
+    del.setAttribute('src', 'img/waste.png');
     section.appendChild(div);
-    section.appendChild(del);
-
-
-
-
+    div.appendChild(span);
+    div.appendChild(del);
 
     input.value = "";
     e.preventDefault();
+
+    del.addEventListener('click', delFun);
 })
+
+function delFun(e){
+    const del = e.target.parentNode;
+    del.remove();
+    
+}
+

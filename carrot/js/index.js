a63146913102a2f67a58f9c1c82683f9ca7ee4b0
Jaeeun98 <wodms4525@naver.com>
2021-06-09 00:11:05 +0900

    Title : Add
    JS, 당근 클릭 시 삭제, 벌레 클릭 시 popup 창 띄우기, replayBtn add

--- carrot/js/index.js
+++ carrot/js/index.js
@@ -1,23 +1,47 @@
-const playBtn = document.querySelector('.play');
+const button = document.getElementsByTagName('button');
 const ul = document.getElementsByTagName('ul')[0];
+const popup = document.querySelector('.popup');
 
 function play(){
-    const li = document.getElementsByTagName('li');
-    
+    const li = document.getElementsByTagName('li')
+
     for(let i=0; i<li.length; i++){
         const randomX = Math.floor(Math.random() * 730);
         const randomY = Math.floor(Math.random() * 130);
 
         li[i].style.transform = `translate(${randomX}px, ${randomY}px)`;
     }  
+    ul.style.display = 'block';
+}
+
+function replay(){
+    const img = document.getElementsByTagName('img');
+
+    for(let i=0; i<img.length-7; i++){
+        img[i].style.display = 'block';
+    }
+    
+    popup.style.display = 'none';
+    play();
 }
 
 function del(e){
-    console.log(e);
+    const result = document.querySelector('.result');
+
+    if(e.target.tagName !== 'IMG') return;
+
+    if(e.target.getAttribute('alt') == 'carrot'){
+        e.target.style.display = 'none';
+    } else {
+        popup.style.display = 'block';
+        result.innerText = `You Loser😂 ` 
+    }
 }
 
-playBtn.addEventListener('click', play)
+button[0].addEventListener('click', play)
+button[1].addEventListener('click', (e) => {
+    replay(e);
+});
 ul.addEventListener('click', (e) => {
-    
-    e.target.style.display = 'none';
+    del(e);
 })
